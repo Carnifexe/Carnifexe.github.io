75faf6c35cc7a92844a2511d470b8f0099ff7298
Carnifexe <Ariadolis@web.de>
2025-03-31 19:53:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,29 +13,51 @@ let players = [];
 let queue = [];
 let rooms = [];
 
+function broadcastQueueCount() {
+  const count = queue.length;
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({
+        type: "queueUpdate",
+        count
+      }));
+    }
+  });
+}
+
 wss.on('connection', (ws) => {
   players.push(ws);
+  broadcastQueueCount();
   
   ws.on('message', (message) => {
     const data = JSON.parse(message);
     
     if (data.type === 'joinQueue') {
-      queue.push(ws);
-      if (queue.length >= 2) {
-        const player1 = queue.shift();
-        const player2 = queue.shift();
-        const room = { players: [player1, player2] };
-        rooms.push(room);
+      if (!queue.includes(ws)) {
+        queue.push(ws);
+        broadcastQueueCount();
         
-        player1.send(JSON.stringify({ 
-          type: 'gameStart', 
-          playerNumber: 1 
-        }));
-        player2.send(JSON.stringify({ 
-          type: 'gameStart', 
-          playerNumber: 2 
-        }));
+        if (queue.length >= 2) {
+          const player1 = queue.shift();
+          const player2 = queue.shift();
+          const room = { players: [player1, player2] };
+          rooms.push(room);
+          
+          player1.send(JSON.stringify({ 
+            type: 'gameStart', 
+            playerNumber: 1 
+          }));
+          player2.send(JSON.stringify({ 
+            type: 'gameStart', 
+            playerNumber: 2 
+          }));
+          broadcastQueueCount();
+        }
       }
+    }
+    else if (data.type === 'leaveQueue') {
+      queue = queue.filter(p => p !== ws);
+      broadcastQueueCount();
     }
     else if (data.type === 'ballUpdate') {
       const room = rooms.find(r => r.players.includes(ws));
@@ -63,6 +85,7 @@ wss.on('connection', (ws) => {
       }
       return true;
     });
+    broadcastQueueCount();
   });
 });
 
