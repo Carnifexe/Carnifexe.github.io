3854686fe24dfabab87091f41e828067d8c99106
Carnifexe <Ariadolis@web.de>
2025-04-02 11:27:16 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,63 +1,20 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const http = require("http");
+const sockets = require("./sockets");
+const io = require("socket.io");
 
-let players = [];
-let playerCount = 0;
+const port = process.env.PORT || 8000;
 
-app.use(express.static('public'));
-
-io.on('connection', (socket) => {
-  console.log('New player connected: ' + socket.id);
-
-  // Spieler zur Liste hinzufügen
-  players.push(socket);
-  playerCount++;
-
-  // Sende die Liste der verbundenen Spieler an alle Clients
-  io.emit('playerList', getWaitingPlayerNames());
-
-  // Wenn ein Spieler eine Herausforderung schickt
-  socket.on('challengePlayer', (challengerName, opponentId) => {
-    const opponentSocket = players.find(player => player.id === opponentId);
-    if (opponentSocket) {
-      opponentSocket.emit('receiveChallenge', challengerName, socket.id);
-    }
-  });
-
-  // Wenn der Gegner eine Herausforderung annimmt oder ablehnt
-  socket.on('challengeResponse', (accept, challengerId) => {
-    const challengerSocket = players.find(player => player.id === challengerId);
-    if (challengerSocket) {
-      if (accept) {
-        challengerSocket.emit('startGame');
-        socket.emit('startGame');
-      } else {
-        challengerSocket.emit('challengeDeclined');
-        socket.emit('challengeDeclined');
-      }
-    }
-  });
-
-  // Wenn ein Spieler das Spiel verlässt
-  socket.on('disconnect', () => {
-    console.log('Player disconnected: ' + socket.id);
-    players = players.filter(player => player.id !== socket.id);
-    // Sende die aktualisierte Liste an alle Clients
-    io.emit('playerList', getWaitingPlayerNames());
-  });
+const apiServer = require("./api");
+const httpServer = http.createServer(apiServer);
+const socketServer = io(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-// Funktion zur Ausgabe der Warteliste
-function getWaitingPlayerNames() {
-  return players.map((player, index) => {
-    return { name: `Spieler ${index + 1}`, id: player.id };
-  });
-}
-
-server.listen(3000, () => {
-  console.log('Server läuft auf http://localhost:3000');
+httpServer.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
+
+sockets.listen(socketServer);
