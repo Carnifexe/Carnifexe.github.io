0d60b7664406b9176a6bffaa1b0ece1c25b0bfce
Carnifexe <Ariadolis@web.de>
2025-04-04 16:47:58 +0200

    Update getData.js

--- bussgeldrechner/api/getData.js
+++ bussgeldrechner/api/getData.js
@@ -1,13 +1,24 @@
 export default async function handler(req, res) {
-  const BIN_ID = process.env.BIN_ID;
-  const API_KEY = process.env.API_KEY;
+    // Hole die Umgebungsvariablen
+    const BIN_ID = process.env.BIN_ID;
+    const API_KEY = process.env.API_KEY;
 
-  const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}`, {
-    headers: {
-      'X-Master-Key': API_KEY
+    try {
+        // Hole die Daten von jsonbin.io
+        const response = await fetch(`https://api.jsonbin.io/v3/b/${BIN_ID}/latest`, {
+            headers: {
+                "X-Master-Key": API_KEY
+            }
+        });
+
+        // Wenn die Anfrage erfolgreich war, sende die Daten als Antwort zurück
+        if (response.ok) {
+            const data = await response.json();
+            res.status(200).json(data);
+        } else {
+            res.status(response.status).json({ error: "Daten konnten nicht abgerufen werden" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Fehler beim Abrufen der Daten" });
     }
-  });
-
-  const data = await response.json();
-  res.status(200).json(data);
 }
